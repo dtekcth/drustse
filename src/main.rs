bc5f8095c3b3d90d935d32a35d44f09a0b27dc8d
bryal <96.bryal@gmail.com>
2016-06-01 18:12:16 +0200

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate hyper;
+
+use hyper::server::{Request, Response};
+
+static HELLO_WORLD: &'static [u8] = b"Hello World!";
+
+fn hello(_: Request, resp: Response) {
+    resp.send(HELLO_WORLD).unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _ = hyper::Server::http("127.0.0.1:3000")
+                .unwrap()
+                .handle(hello);
+
+    println!("Listening on http://127.0.0.1:3000");
 }
