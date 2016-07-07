42feb3514e39a168a17b393cb244046014fbfee8
bryal <96.bryal@gmail.com>
2016-07-08 01:48:23 +0200

    use nickel as webserver/framework instead of iron

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
-extern crate hyper;
+extern crate nickel;
 
-use hyper::server::{Request, Response};
-
-static HELLO_WORLD: &'static [u8] = b"Hello World!";
-
-fn hello(_: Request, resp: Response) {
-    resp.send(HELLO_WORLD).unwrap();
-}
+use nickel::{Nickel, HttpRouter};
+use std::sync::{ Arc, Mutex };
 
 fn main() {
-    let _ = hyper::Server::http("192.168.0.40:8040")
-                .unwrap()
-                .handle(hello);
+    let mut visitor_count: u64 = 0;
 
-    println!("Listening on http://192.168.0.40:8040");
+    let shared_count = Arc::new(Mutex::new(visitor_count));
+
+    let server = Nickel::new();
+
+    server.listen("127.0.0.1:8040");
+
+
+
+    server.get("/", middleware! { |_, res|
+        let mut data = HashMap::<&str, &str>::new();
+        data.insert("name", "user");
+        return res.render("examples/assets/template.tpl", &data)
+    });
+
+    println!("Listening on http://127.0.0.1:8040");
 }
