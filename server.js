0118105fb85704a4a7bd6e060a87c249f7049efd
tairoman <ygge95@gmail.com>
2016-07-08 00:35:36 +0200

    Added some comments

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 'use strict';
 
-const express = require('express');
 const sassMiddleware = require('node-sass-middleware');
+const express = require('express');
+
+const app = express();
 
 // For ES6 js in pug files
 const pug = require('pug');
 const babel = require('jade-babel');
 pug.filters.babel = babel({});
 
-
-const app = express();
+// Constants
 const debugMode = true;
 const port = 5000;
 
-
+// Settings for static files
 app.use(sassMiddleware({
   src: __dirname + "/public",
   dest:__dirname + "/public",
@@ -22,14 +23,15 @@ app.use(sassMiddleware({
 
 app.use(express.static('public'));
 
-
+// For templates
 app.set('view engine', 'pug');
 
-app.get('/',      (req, res) => { res.render('base'); });
-app.get('/drust', (req, res) => { res.render('drust', {title:'DRust'}); });
-app.get('/basen', (req, res) => { res.render('basen', {title:'Basen'}); });
-app.get('/verktyg', (req, res) => { res.render('verktyg', {title:'Verktyg'}); });
+// Routes
+app.get('/',          (req, res) => { res.render('base'); }); // Placeholder template
+app.get('/drust',     (req, res) => { res.render('drust', {title:'DRust'}); });
+app.get('/basen',     (req, res) => { res.render('basen', {title:'Basen'}); });
+app.get('/verktyg',   (req, res) => { res.render('verktyg', {title:'Verktyg'}); });
 app.get('/automaten', (req, res) => { res.render('automaten', {title:'Automaten'}); });
-app.get('/flipper', (req, res) => { res.render('flipper', {title:'Flipper'}); });
+app.get('/flipper',   (req, res) => { res.render('flipper', {title:'Flipper'}); });
 
 app.listen(port);
