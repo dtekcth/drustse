022751007c5585aca830ec94860859e9d6740f0f
tairoman <ygge95@gmail.com>
2016-07-14 00:30:56 +0200

    Can now update and create new items

--- toolModel.js
+++ toolModel.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
-const VerktygSchema = Schema({
+const ToolSchema = Schema({
   name:   { type: String/*,   required: true */},
   amount: { type: Number/*,   required: true */},
   id:     { type: ObjectId/*, required: true */}
 });
 
-const Verktyg = mongoose.model('Verktyg', VerktygSchema);
+const Tool = mongoose.model('Tool', ToolSchema);
 
-module.exports = Verktyg;
+module.exports = Tool;
